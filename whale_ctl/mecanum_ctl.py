797accbf4d768eefda988e3c0fdede6db6bff11a
NQZQMI <nozomixiaochi79@gmail.com>
2025-06-26 12:32:54 +0900

    update matrix

--- whale_ctl/mecanum_ctl.py
+++ whale_ctl/mecanum_ctl.py
@@ -4,10 +4,10 @@ def generate_motor_cmd(v_x, v_y, v_yaw):
 
 
     A = numpy.array([
-        [ 1,  1,  1],   # motor_0: Front Left
-        [-1,  1, -1],   # motor_1: Front Right
-        [-1,  1,  1],   # motor_2: Rear Left
-        [ 1,  1, -1]    # motor_3: Rear Right
+        [ 1,  1,  1],   # motor_0: Front Right
+        [ 1, -1,  1],   # motor_1: Front Left
+        [-1, -1,  1],   # motor_2: Rear Left
+        [-1,  1,  1]    # motor_3: Rear Right
     ])
 
     Vin = numpy.array([v_x, v_y, v_yaw])
